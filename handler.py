82f9b4ab99b7ad0bae8e6081045fdd37b105f16b
yueda <chua_yueda@hotmail.com>
2021-03-25 07:06:54 +0800

    updated handler to calculate greeks and clean up template

--- handler.py
+++ handler.py
@@ -15,12 +15,31 @@ def gordon_growth(stock, req_return):
     result = list()
     result.append(("Required Rate Of Return", round(req_return, 3)))
     growth_rate = stock.growth_rate()
+    result.append(("Growth Rate", round(growth_rate, 3)))
     print(req_return)
     value = gordon_growth_valuation(stock.full_year_dividend(), req_return, growth_rate)
     result.append(("Valuation", value))
     return result
 
+def get_greeks(s, market, rf):
+    logging.debug("Calculating Greeks")
+    end = datetime.now()
+    end_string = end.strftime("%Y-%m-%d")
+    start = datetime(end.year - 1, end.month, end.day)
+    start_string = start.strftime("%Y-%m-%d")  
+    logging.debug(f"Calculating greeks using data from {start_string} to {end_string}")
 
+    rf_history = rf.yield_history(start_string, end_string, "monthly")[1:] #Drop current spot yield
+    sortino = s.get_sortino_ratio(start_string, end_string, "monthly", rf_history)
+    sharpe = s.get_sharpe_ratio_ex_post(start_string, end_string, "monthly", rf_history)
+    cv = s.get_coefficient_of_variation(1, "monthly")
+    market_one_year_return = market.get_annualised_return(1)
+    alpha = s.get_alpha(1, market_one_year_return, rf_history[0])
+    return {"sortino": sortino, "sharpe": sharpe, "cv": cv, "alpha": alpha}
+
+
+def __convert_to_pct(val):
+    return round(val * 100, 3)
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG )
@@ -34,19 +53,21 @@ if __name__ == "__main__":
     s3_bucket = config.get("s3_bucket")
     s3_report_dir = config.get("s3_report_dir")
     logging.info(f"""Initialised application
-    Using following configurations: S3 Bucket: {s3_bucket}  S3 Report Dir: {s3_report_dir} Report template: {report_template}""")
+Using following configurations: S3 Bucket: {s3_bucket}  S3 Report Dir: {s3_report_dir} Report template: {report_template}""")
     
     s = Stock("C31.SI")
     rf = RiskFree(10)
     market = Market(s.get_stock_exchange())
     req_rate = CAPM(rf.spot_yield, market.get_annualised_return(rf.year), s.beta)
+    greeks = get_greeks(s, market, rf)
 
     gg_result = gordon_growth(s, req_rate)
 
     report_name = f"{s.stock_code}-report.html"
     
     template = util.get_template("report_template.html")
-    template_vars = {"gordon_growth": gg_result, "timestamp": datetime.now().strftime("%Y:%m:%d")}
+    template_vars = {"spot_yield": __convert_to_pct(rf.spot_yield), "market_ann_return": __convert_to_pct( market.get_annualised_return(rf.year)), "benchmark_year": rf.year, "req_rate": __convert_to_pct(req_rate),
+                    "greeks": greeks, "gordon_growth": gg_result, "timestamp": datetime.now().strftime("%Y:%m:%d")}
 
     html_out = template.render(template_vars, s = s)
     logging.debug("Generated report file")
